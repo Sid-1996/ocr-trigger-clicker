10cb8aa69dc4e67aafa483abd0eb5e083711a481
Sid <as1000028@gmail.com>
2026-07-08 04:26:11 +0800

    feat: 工具列新增「任務分享」下拉選單（瀏覽分享區＋分享目前任務）

--- gui/06_gui_main.py
+++ gui/06_gui_main.py
@@ -2708,6 +2708,10 @@ class MainWindow(QMainWindow):
         toolbar.addWidget(self._task_del_btn)
         toolbar.addWidget(self._task_import_btn)
         toolbar.addWidget(self._task_export_btn)
+        self._task_share_btn = QPushButton("📂任務分享")
+        self._task_share_btn.setToolTip("瀏覽社群分享的任務檔案，或分享自己的任務設定")
+        self._task_share_btn.clicked.connect(self._open_task_share_menu)
+        toolbar.addWidget(self._task_share_btn)
 
         # -- Action section --
         self._btn_toggle = QPushButton("啟動")
@@ -5450,6 +5454,30 @@ class MainWindow(QMainWindow):
         import webbrowser
 
         webbrowser.open(_GUIDE_URL)
+
+    def _open_task_share_menu(self):
+        import webbrowser
+
+        from PyQt6.QtCore import QPoint
+
+        menu = QMenu(self)
+        menu.addAction("🌐 瀏覽任務分享區").triggered.connect(
+            lambda: webbrowser.open(
+                "https://github.com/Sid-1996/ocr-trigger-clicker/discussions/categories/"
+                "%E4%BB%BB%E5%8B%99%E6%AA%94%E6%A1%88%E5%88%86%E4%BA%AB"
+            )
+        )
+        menu.addAction("📤 分享目前任務").triggered.connect(lambda: self._export_and_share())
+        menu.exec(self._task_share_btn.mapToGlobal(QPoint(0, self._task_share_btn.height())))
+
+    def _export_and_share(self):
+        import webbrowser
+
+        self._on_task_export()
+        webbrowser.open(
+            "https://github.com/Sid-1996/ocr-trigger-clicker/discussions/categories/"
+            "%E4%BB%BB%E5%8B%99%E6%AA%94%E6%A1%88%E5%88%86%E4%BA%AB"
+        )
 
     def _check_version(self):
         import urllib.request
